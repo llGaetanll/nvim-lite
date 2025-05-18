633871b34b6d2340c32ee09f3618e6194d7167f5
Gaetan Almela <gaetan.almela@gmail.com>
2025-05-18 17:49:31 -0400

    Change default theme to default-dark
    default-dark has the best color semantics. Something you expect to be
    red is red, something green is green, and so on. This is important for
    LSP messages or diff colors.

--- lua/plugins/base16.lua
+++ lua/plugins/base16.lua
@@ -3,7 +3,7 @@ return {
         "llGaetanll/base16.nvim",
         priority = 1000,
         opts = {
-            default_theme = "everforest",
+            default_theme = "default-dark",
             on_change = require "highlights"
         },
     },
