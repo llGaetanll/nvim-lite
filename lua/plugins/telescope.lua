41f342f25d7a23b5fdaa45c2e71ca4092a68d5e8
Gaetan Almela <gaetan.almela@gmail.com>
2025-05-18 10:40:53 -0400

    Startup with find_files instead of file_browser

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -27,8 +27,38 @@ return {
         tag = "0.1.8",
         dependencies = { "nvim-lua/plenary.nvim" },
         config = function()
-            local fb_actions = require "telescope".extensions.file_browser.actions
+            -- clear FileExplorer appropriately to prevent netrw from launching on folders
+            -- netrw may or may not be loaded before telescope-find-files
+            -- conceptual credits to nvim-tree and telescope-file-browser
+            local find_files_hijack_netrw = vim.api.nvim_create_augroup("find_files_hijack_netrw", { clear = true })
+            vim.api.nvim_create_autocmd("VimEnter", {
+                pattern = "*",
+                once = true,
+                callback = function()
+                    pcall(vim.api.nvim_clear_autocmds, { group = "FileExplorer" })
+                end,
+            })
 
+            vim.api.nvim_create_autocmd("BufEnter", {
+                group = find_files_hijack_netrw,
+                pattern = "*",
+                callback = function()
+                    vim.schedule(function()
+                        -- Early return if netrw or not a directory
+                        if vim.bo[0].filetype == "netrw" or vim.fn.isdirectory(vim.fn.expand("%:p")) == 0 then
+                            return
+                        end
+
+                        vim.api.nvim_buf_set_option(0, "bufhidden", "wipe")
+
+                        require("telescope.builtin").find_files({
+                            cwd = vim.fn.expand("%:p:h"),
+                        })
+                    end)
+                end,
+            })
+
+            local fb_actions = require "telescope".extensions.file_browser.actions
             require("telescope").setup({
                 defaults = {
                     selection_caret = " ",
@@ -71,7 +101,6 @@ return {
                 extensions = {
                     file_browser = {
                         theme = "dropdown",
-                        hijack_netrw = true,
                         hide_parent_dir = true,
                         grouped = true,
                         sorting_strategy = "ascending",
